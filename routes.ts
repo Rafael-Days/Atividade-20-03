16acd94f18f64af5a863645c558b46b3c081462f
Rafael Days <130679216+Rafael-Days@users.noreply.github.com>
2024-04-08 10:39:56 -0300

    routes

--- routes.ts
+++ routes.ts
@@ -12,17 +12,17 @@ routes.get('/usuario/:id', usuarioController.findById)
 routes.put('/usuario/:id', usuarioController.update)
 routes.delete('/usuario/:id', usuarioController.delete)
 //TAREFA
-routes.post('/tarefa', tarefaController.create)
+routes.post('/tarefa', tarefaController.create)//criação de uma nova tarefa
 routes.get('/tarefa', tarefaController.findAll)
-routes.get('/tarefa/:id', tarefaController.findById)
-routes.put('/tarefa/:id', tarefaController.update)
-routes.delete('/tarefa/:id', tarefaController.delete)
+routes.get('/tarefa/:id', tarefaController.findById)//Rota para obter detalhes de uma tarefa específica
+routes.put('/tarefa/:id', tarefaController.update)//Rota para atualizar uma tarefa existente
+routes.delete('/tarefa/:id', tarefaController.delete)//Rota para excluir uma tarefa.
 //CATEGORIA
-routes.post('/categoria', categoriaControllers.create)
+routes.post('/categoria', categoriaControllers.create)//Rota para criação de uma nova categoria.
 routes.get('/categoria', categoriaControllers.findAll)
-routes.get('/categoria/:id', categoriaControllers.findById)
-routes.put('/categoria/:id', categoriaControllers.update)
-routes.delete('/categoria/:id', categoriaControllers.delete)
+routes.get('/categoria/:id', categoriaControllers.findById)//Rota para obter detalhes de uma categoria específica
+routes.put('/categoria/:id', categoriaControllers.update)//Rota para atualizar uma categoria existente
+routes.delete('/categoria/:id', categoriaControllers.delete)//Rota para excluir uma categoria.
 
 export{
     routes
